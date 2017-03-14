8e665e5a903cfcc67d9c1d1a9fb15dadd7df0dea
polozovaanastasia <polozova.anastasia@gmail.com>
2017-03-14 23:07:39 +0500

    1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('.header-top .head-nav-toggle').click(function(){
+  		$('.header-top .head-nav').slideToggle(600);
+	});
+
+	$('.portfolio .head-nav-toggle').click(function(){
+  		$('.portfolio .head-nav').slideToggle(600);
+	});
+	
+});
